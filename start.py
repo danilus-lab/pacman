aa8d8f51d9e14a0fae998eee1fe83bcd9993626b
danilus-lab <daniil.novsky@yandex.ru>
2020-01-02 15:58:39 +0300

    start screen

--- start.py
+++ start.py
@@ -20,17 +20,49 @@ def load_image(name, colorkey=None):
 
 
 def start_screen():
-    screen.fill((0, 0, 0))
-    font = pygame.font.Font(None, 50)
-    text = font.render("PACMAN", 1, (100, 255, 100))
-    text_x = width // 2 - text.get_width() // 2
-    text_y = height // 2 - text.get_height() // 2 - 250
-    text_w = text.get_width()
-    text_h = text.get_height()
-    screen.blit(text, (text_x, text_y))
-    menu = font.render("Menu", 1, (100, 255, 100))
-    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-                                           text_w + 20, text_h + 20), 1)
+    running = True
+    all_sprites = pygame.sprite.Group()
+    arrow = pygame.sprite.Sprite(all_sprites)
+    arrow.image = load_image('arrow.png', -1)
+    arrow.rect = arrow.image.get_rect()
+    arrow.rect.x = 20
+    arrow.rect.y = 30
+
+    image = pygame.sprite.Sprite(all_sprites)
+    image.image = load_image('start.jpg')
+    image.rect = arrow.image.get_rect()
+    image.rect.x = 350
+    image.rect.y = 400
+    while running:
+        screen.fill((0, 0, 0))
+        font = pygame.font.Font(None, 50)
+        text = font.render("PACMAN", 1, (123, 104, 238))
+        text_x = width // 2 - text.get_width() // 2
+        text_y = height // 2 - text.get_height() // 2 - 250
+        text_w = text.get_width()
+        text_h = text.get_height()
+        screen.blit(text, (text_x, text_y))
+        menu = font.render("PLAY", 1, (100, 255, 255))
+        screen.blit(menu, (text_x + 30, text_y + 100))
+        exit = font.render("EXIT", 1, (100, 255, 255))
+        screen.blit(exit, (text_x + 33, text_y + 200))
+        pygame.draw.rect(screen, (0, 0, 255), (text_x - 10, text_y - 10,
+                                               text_w + 20, text_h + 20), 1)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    if arrow.rect.y < 50:
+                        arrow.rect.y += 100
+                elif event.key == pygame.K_UP:
+                    if arrow.rect.y > 105:
+                        arrow.rect.y -= 100
+        all_sprites.update()
+        all_sprites.draw(screen)
+        pygame.display.flip()
+
+
 
 
 def terminate():
